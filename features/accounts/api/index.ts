3f82e63c50e71e6b481c9e072acd8abaf93ac4b6
ttnguyen115 <thanhtrung1012000@gmail.com>
2024-05-12 16:05:41 +0700

    feat: add delete action

--- features/accounts/api/index.ts
+++ features/accounts/api/index.ts
@@ -1,5 +1,6 @@
 export { default as useBulkDeleteAccounts } from "./useBulkDeleteAccounts";
 export { default as useCreateAccount } from "./useCreateAccount";
+export { default as useDeleteAccount } from "./useDeleteAccount";
 export { default as useEditAccount } from "./useEditAccount";
 export { default as useGetAccountById } from "./useGetAccountById";
 export { default as useGetAccounts } from "./useGetAccounts";
--- features/accounts/components/EditAccountSheet.tsx
+++ features/accounts/components/EditAccountSheet.tsx
@@ -1,5 +1,6 @@
 // hooks
 import { useOpenAccount } from "@/features/accounts/hooks";
+import { useConfirm } from "@/hooks";
 
 // components
 import {
@@ -16,43 +17,63 @@ import { Loader2 } from "lucide-react";
 import { type FormValues } from "./AccountForm";
 
 // apis
-import { useEditAccount, useGetAccountById } from "@/features/accounts/api";
+import { useDeleteAccount, useEditAccount, useGetAccountById } from "@/features/accounts/api";
 
 const EditAccountSheet = () => {
     const { isOpen, onClose, id } = useOpenAccount();
 
-    const { data: account, isLoading } = useGetAccountById(id);
-    const { mutate, isPending } = useEditAccount(id);
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You are about to delete this transaction"
+    );
 
+    const { data: account, isLoading } = useGetAccountById(id);
+    const { mutate: editMutation, isPending: isEditing } = useEditAccount(id);
+    const { mutate: deleteMutation, isPending: isDeleting } = useDeleteAccount(id);
+
+    const isPending = isEditing || isDeleting;
     const defaultAccountFormValues = account ? { name: account.name } : { name: "" };
 
     const onSubmit = (values: FormValues) => {
-        mutate(values, {
+        editMutation(values, {
             onSuccess: onClose,
         });
     };
 
+    const onDelete = async () => {
+        const ok = await confirm();
+        if (ok) {
+            deleteMutation(undefined, {
+                onSuccess: onClose,
+            })
+        }
+    }
+
     return (
-        <Sheet open={isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
-                    <SheetTitle>Edit Account</SheetTitle>
-                    <SheetDescription>Edit an existing account</SheetDescription>
-                </SheetHeader>
-                {isLoading ? (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <Loader2 className="size-4 text-muted-foreground animate-spin" />
-                    </div>
-                ) : (
-                    <AccountForm
-                        id={id}
-                        onSubmit={onSubmit}
-                        disabled={isPending}
-                        defaultValues={defaultAccountFormValues}
-                    />
-                )}
-            </SheetContent>
-        </Sheet>
+        <>
+            <ConfirmDialog />
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent className="space-y-4">
+                    <SheetHeader>
+                        <SheetTitle>Edit Account</SheetTitle>
+                        <SheetDescription>Edit an existing account</SheetDescription>
+                    </SheetHeader>
+                    {isLoading ? (
+                        <div className="absolute inset-0 flex items-center justify-center">
+                            <Loader2 className="size-4 text-muted-foreground animate-spin" />
+                        </div>
+                    ) : (
+                        <AccountForm
+                            id={id}
+                            onSubmit={onSubmit}
+                            onDelete={onDelete}
+                            disabled={isPending}
+                            defaultValues={defaultAccountFormValues}
+                        />
+                    )}
+                </SheetContent>
+            </Sheet>
+        </>
     );
 };
 
