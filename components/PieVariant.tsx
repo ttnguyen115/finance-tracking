62715e1c9518be3f884b92a0c8ea1633f86f0bce
ttnguyen115 <thanhtrung1012000@gmail.com>
2025-04-05 15:10:11 +0700

    change PieVariant and add pm2 for vps

--- components/PieVariant.tsx
+++ components/PieVariant.tsx
@@ -10,6 +10,23 @@ import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recha
 const COLORS = ["#0062FF", "#12C6FF", "#FF647F", "#FF9354"];
 
 const PieVariant = ({ data }: PieVariantProps) => {
+    const renderLegendContent = ({ payload }: any) => {
+        return (
+            <ul className="flex flex-col space-y-2">
+                {/* TODO: fix these `any` */}
+                {payload.map((entry: any, index: number) => (
+                    <li key={`item-${index}`} className="flex items-center space-x-2">
+                        <span className="size-2 rounded-full" style={{ backgroundColor: entry.color }} />
+                        <div className="space-x-1">
+                            <span className="text-sm text-muted-foreground">{entry.value}</span>
+                            <span className="text-sm">{formatPercentage(entry.payload.percent * 100)}</span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        );
+    };
+
     return (
         <ResponsiveContainer width="100%" height={350}>
             <PieChart>
@@ -18,27 +35,7 @@ const PieVariant = ({ data }: PieVariantProps) => {
                     verticalAlign="bottom"
                     align="right"
                     iconType="circle"
-                    content={({ payload }: any) => {
-                        return (
-                            <ul className="flex flex-col space-y-2">
-                                {/* TODO: fix these `any` */}
-                                {payload.map((entry: any, index: number) => (
-                                    <li key={`item-${index}`} className="flex items-center space-x-2">
-                                        <span
-                                            className="size-2 rounded-full"
-                                            style={{ backgroundColor: entry.color }}
-                                        />
-                                        <div className="space-x-1">
-                                            <span className="text-sm text-muted-foreground">{entry.value}</span>
-                                            <span className="text-sm">
-                                                {formatPercentage(entry.payload.percent * 100)}
-                                            </span>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        );
-                    }}
+                    content={renderLegendContent}
                 />
                 <Tooltip content={<CategoryTooltip />} />
                 <Pie
